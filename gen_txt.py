252bd977dc523b4328a3a1c00793651892217899
Xzz2296 <2014220336@qq.com>
2023-12-19 14:19:28 +0800

    gen_txt

---
+++ gen_txt.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import os
+from tqdm import tqdm  # 导入tqdm模块
+
+# 读取Excel文件
+excel_file = r'E:\xpj\research\POI\desc_poi3.csv'  # 替换成你的Excel文件路径
+df = pd.read_csv(excel_file)
+
+
+# 获取总行数
+total_rows = len(df)
+
+# 指定保存文件的目录
+output_directory = 'caption_prompt'  # 替换成你想要保存文件的目录路径
+
+# 使用tqdm显示进度
+for index, row in tqdm(df.iterrows(), total=total_rows, desc="生成.txt文件进度"):
+    file_name = os.path.splitext(row['image'])[0] + '.txt'  # 去掉扩展名并添加.txt扩展名
+    if file_name =="#NAME?.txt" :
+        continue
+    text = (str(row['prompt']) if pd.notna(row['prompt']) else '')
+    # text = (str(row['poi_prompt']) if pd.notna(row['poi_prompt']) else '') +'.' +(str(row['prompt']) if pd.notna(row['prompt']) else '')
+
+    # text = row['prompt']+row['poi_prompt']  # 获取文本内容
+
+    # 创建并写入txt文件
+    file_path = os.path.join(output_directory, file_name)
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write(text)
+
+print("生成.txt文件完成。")
