306e25c89950c72a31a28176c1d720bdc9760de5
Xzz2296 <2014220336@qq.com>
2023-12-21 12:46:16 +0800

    history

---
+++ .history/SQL/poi_dis2_20231221124505.sql
@@ -0,0 +1,18 @@
+CREATE TABLE beijing_img_poi AS
+SELECT
+  img_key PRIMARY KEY,
+  id,
+  distance
+FROM (
+  SELECT
+    i.img_key,
+    p.id,
+    ST_Distance(p.geom::geography, ST_MakePoint(CAST(i.longitude AS double precision), CAST(i.latitude AS double precision))::geography) AS distance,
+    ROW_NUMBER() OVER(PARTITION BY i.img_key ORDER BY ST_Distance(p.geom::geography, ST_MakePoint(CAST(i.longitude AS double precision), CAST(i.latitude AS double precision))::geography)) AS rn
+  FROM
+    beijing_img_location i,
+    beijing p
+  WHERE
+    ST_DWithin(p.geom::geography, ST_MakePoint(CAST(i.longitude AS double precision), CAST(i.latitude AS double precision))::geography, 500)
+) t
+WHERE rn <= 5;
