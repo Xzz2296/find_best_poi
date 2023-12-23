d21bdf9b34b1303e0254d8631c796c98d928a128
Xzz2296 <2014220336@qq.com>
2023-12-23 23:22:08 +0800

    避免下载其他table

--- .history/SQL/auto_download_20231223231805.sh
+++ .history/SQL/auto_download_20231223231805.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 tables=$(psql -U svail -d svaildb -t -c "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
 for table in $tables; do
-    if [[ "$table" != *geodb* && "$table" != *_info* && "$table" != *keyword* && "$table" != *contact*]]; then
+    if [[ "$table" != *geodb* && "$table" != *_info* ]]; then
         psql -U svail -d svaildb -c "\copy (SELECT * FROM $table) TO '/workspace/xpj/csv_result/$table.csv' WITH CSV HEADER"
     fi
 done
